af642f28a2c4c3f831f0618ee42a5445c2e8cfa4
pogonya <pogonya2020@protonmail.com>
2021-03-25 23:32:38 +0200

    migrations + models

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
@@ -14,20 +15,28 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     use Authenticatable, Authorizable, HasFactory;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'email',
-    ];
+    protected $fillable = ['email'];
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $hidden = [
-        'password',
-    ];
+    protected $casts = ['active' => 'boolean'];
+
+    public function polls(): HasMany
+    {
+        return $this->hasMany(Poll::class, 'user_id');
+    }
 }
