b9fadc093e8a8e170b06e10282f07bf1e2fe4240
pogonya <pogonya2020@protonmail.com>
2021-05-24 17:18:37 +0300

    - add InvitationCreated notification - add SendInvitations job - update EmailListController - update PollController - add EmailListResource - add PollResource

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 
@@ -30,5 +31,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        JsonResource::withoutWrapping();
     }
 }
